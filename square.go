4f62c1e5bb3b86de6b946ecd274086755f5eaa5c
english2two <english2two@gmail.com>
2022-07-01 16:35:00 +0600

    version 1.0

--- square.go
+++ square.go
@@ -9,14 +9,20 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func (s Square) End() Point {
+	// implement me\
+	return Point{
+		s.start.x * int(s.a),
+		s.start.y * int(s.a),
+	}
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
+func (s Square) Perimeter() uint {
 	// implement me
+	return s.a * 4
 }
